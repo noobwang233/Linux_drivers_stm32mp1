f9884f1537fecd41df9e87576ebfb0100a4dc6cc
noobwang233 <1213236870@qq.com>
2023-10-21 00:31:25 -0700

    delete useless files

--- Drivers/.leddrv.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/wt/Linux/Linux_drivers/Drivers/leddrv.o := arm-none-linux-gnueabihf-ld  -EL    -r -o /home/wt/Linux/Linux_drivers/Drivers/leddrv.o /home/wt/Linux/Linux_drivers/Drivers/testdrv.o /home/wt/Linux/Linux_drivers/Drivers/led.o
--- Drivers/leddrv.mod
+++
@@ -1,2 +0,0 @@
-/home/wt/Linux/Linux_drivers/Drivers/testdrv.o /home/wt/Linux/Linux_drivers/Drivers/led.o
-
